6a92b2ede2711c3430fd06a36eb38c9fd666b1ce
notShadowM <maanabahin2001@gmail.com>
2021-08-04 13:05:13 +0300

    small changes

--- src/components/MainPage/index.jsx
+++ src/components/MainPage/index.jsx
@@ -1,12 +1,28 @@
-import React from 'react';
+import React,{useEffect, useState} from 'react';
 import {Container} from "./style";
 import { ReactComponent as Cover } from '../../assets/Cover.svg';
 import { ReactComponent as Avatar } from '../../assets/Avatar.svg';
 import { Icon, InlineIcon } from '@iconify/react';
 import heartIcon from '@iconify-icons/akar-icons/heart';
 import shareForwardLine from '@iconify-icons/ri/share-forward-line';
+import heartFilled from '@iconify/icons-ant-design/heart-filled';
 
 export default function MainPage() {
+
+  const [follow,SetFollow] = useState("follow");
+  const [love,setLove] = useState(false);
+
+  const HeartBtn = () =>{
+    if(love){
+      return(
+        <Icon icon={heartFilled} className="icons"/>
+      )
+    }else{
+      return(
+        <Icon icon={heartIcon} className="icons"/>
+      )
+    }
+  }
   return (
     <Container>
       <Cover className="img"/>
@@ -15,7 +31,7 @@ export default function MainPage() {
           <Avatar />
           <div className="username">Alaa Hijazi</div>
         </div>
-        <button className="btn follow">follow</button>
+        <button className="btn follow" onClick={()=>{follow==="follow"?SetFollow("following"):SetFollow("follow")}}>{follow}</button>
       </div>
       <div className="texts-block">
         <p className="headline">
@@ -24,8 +40,8 @@ export default function MainPage() {
         <p className="light-text">Whatever you say, Hello</p>
       </div>
       <div className="interaction-block">
-        <button className="interaction-btns">
-          <Icon icon={heartIcon} className="icons"/>
+        <button className="interaction-btns" onClick={()=>{setLove((prev)=>!prev)}}>
+          <HeartBtn />
         </button>
         <button className="interaction-btns">
           <Icon icon={shareForwardLine} className="icons" flip="horizontal"/>
